5706418108c94cf2ec1b8621edb96f8ee0ad335f
Eric Gorr <ericgorr@mac.com>
2014-02-13 14:46:36 -0500

    Initial Commit

---
+++ Autoload/ELIZDocument.h
@@ -0,0 +1,13 @@
+//
+//  ELIZDocument.h
+//  Autoload
+//
+//  Created by Eric Gorr on 2/13/14.
+//  Copyright (c) 2014 Eric Gorr. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ELIZDocument : NSPersistentDocument
+
+@end
---
+++ AutoloadTests/AutoloadTests.m
@@ -0,0 +1,34 @@
+//
+//  AutoloadTests.m
+//  AutoloadTests
+//
+//  Created by Eric Gorr on 2/13/14.
+//  Copyright (c) 2014 Eric Gorr. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoloadTests : XCTestCase
+
+@end
+
+@implementation AutoloadTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
